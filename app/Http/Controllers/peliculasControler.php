3123f93aa2d8d60bb1353bd4e84dc4c32172e55d
Agustin Biagi <agustin.biagi@gmail.com>
2017-11-05 09:57:40 -0300

    Practica Laravel - ht Ej Clase2.2.a

---
+++ app/Http/Controllers/peliculasControler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class peliculasControler extends Controller
+{
+    function buscarPeliculaId($id){
+         $peliculas=[
+              1=>'Toy Story',
+              2=>'Buscando a Nemo',
+              3=>'Avatar',
+              4=>'StarWars V',
+              5=>'UP',
+              6=>'Mary and Max'
+         ];
+         return 'La Pelicula elegida es: '.$peliculas[$id];
+         }
+
+     /*function buscarPeliculaNombre($nombre){
+          $peliculas=[
+              1=>'Toy Story',
+              2=>'Buscando a Nemo',
+              3=>'Avatar',
+              4=>'StarWars V',
+              5=>'UP',
+              6=>'Mary and Max'
+         ];
+
+         foreach ($peliculas as $key => $titulo) {
+              if ($nombre) {
+                   # code...
+              }
+         }
+    }*/
+
+    public function listado(){
+         $peliculas=[
+             1=>'Toy Story',
+             2=>'Buscando a Nemo',
+             3=>'Avatar',
+             4=>'StarWars V',
+             5=>'UP',
+             6=>'Mary and Max'
+         ];
+         $data = compact("peliculas");
+
+         return view("Peliculas", $data);
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('miPrimerRuta', function(){
+     return 'Cree mi primera ruta';
+});
+
+Route::get('resultado/{numero}', function($numero){
+     if ($numero%2==0) {
+          return 'el numero es par';
+     }else {
+          return 'el numero es impar';
+     }
+});
+
+Route::get('resultado2/{numero}/{numero2?}', function($numero, $numero2=null){
+     if ($numero2) {
+          return $numero*$numero2;
+     }else{
+          if ($numero%2==0) {
+               return 'el numero es par';
+          }else {
+               return 'el numero es impar';
+          }
+}
+});
+
+Route::get('/peliculas/{id}', 'peliculasControler@buscarPeliculaId');
+Route::get('/peliculas', 'peliculasControler@listado');
